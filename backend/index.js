52545380176464e66f7f81b9e6fad72c081d7c6d
Rafael bastidas <rafaelbastidas93@gmail.com>
2020-09-10 21:56:49 -0400

    10-09-2020: Modelo de entrega

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const morgan = require('morgan');
+//const morgan = require('morgan');
 const express = require('express');
 const path = require('path');
 //const cors = require('cors');
@@ -10,7 +10,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 
 //Middlewares
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 //app.use(cors());
